13e3775e227e8b39a6ce29d969df659c28485a27
Mihail <kasdevia@gmail.com>
2024-12-18 06:44:30 +0400

    added scss

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
+/* eslint-disable prettier/prettier */
+import { defineConfig } from 'vite';
 
-export default defineConfig ({
+export default defineConfig({
     root: './src',
     server: {
         port: 3000,
         open: true,
     },
     build: {
-        outDir: '../build'
-    }
-})
+        outDir: '../build',
+        emptyOutDir: true,
+    },
+});
