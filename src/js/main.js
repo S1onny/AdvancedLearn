1b91229ad58414583d0c6bdce554269fc7c089b2
Mihail <kasdevia@gmail.com>
2025-01-17 23:01:03 +0400

    Created hero section and fixed header

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ import '../assets/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { headerScroll } from './components/scroll.js';
+// import { headerScroll } from './components/scroll.js';
 
 useTheme();
 useBurger();
-headerScroll();
+// headerScroll();
