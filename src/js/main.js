b00da93b7799b6a431104d4ab655460166f37c23
Mihail <kasdevia@gmail.com>
2025-01-22 21:25:23 +0400

    Created insight section and slider

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import '../assets/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider } from './components/slider.js';
 // import { headerScroll } from './components/scroll.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
 // headerScroll();
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
@@ -6,16 +5,19 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     root: './src',
+    base: '/coca/',
     server: {
         port: 3000,
-        open: true,
     },
+    publicDir: './src/assets',
     build: {
-        outDir: '../build',
+        outDir: '../dist',
         emptyOutDir: true,
         rollupOptions: {
             input: {
                 main: './src/index.html',
+                pricing: './src/pricing.html',
+                contact: './src/contact.html',
             },
         },
     },
