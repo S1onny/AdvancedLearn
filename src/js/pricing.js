4ee3e47beec73edc3d11113dad713c6eb70e4ec4
Mihail <kasdevia@gmail.com>
2025-02-01 12:46:34 +0400

    Create half of main pricing

---
+++ src/js/pricing.js
@@ -0,0 +1,9 @@
+import '../assets/scss/pricing.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();
+// headerScroll();
